25d6186bc890c8a826a67b1a6cbfa8d8188256ef
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2013-10-24 17:45:49 +0600

    Added some delay, just for test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 /* Can driver
 Author: Mikhail Chernoskutov
 */
+#define F_CPU 10000000
 #include <avr/io.h>
 #include "portwork.cpp"
 #include "canbus.cpp"
+#include <util/delay.h>
 
 int main(void){
     setBit(DDRB, 0);
     while(true){
         switchBit(PORTB, 0);
+        _delay_ms(1000);
     }
 }
 
