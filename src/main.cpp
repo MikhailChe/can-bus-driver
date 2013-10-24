a18402da793aba4a1be0c604f7a59e30c87405a8
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2013-10-24 18:03:38 +0600

    Some other tests, yes I know I should be using another branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /* Can driver
 Author: Mikhail Chernoskutov
 */
-#define F_CPU 10000000
+#define F_CPU 1000000
 #include <avr/io.h>
 #include "portwork.cpp"
 #include "canbus.cpp"
@@ -9,9 +9,21 @@ Author: Mikhail Chernoskutov
 
 int main(void){
     setBit(DDRB, 0);
+    double sleep = 0;
+    int direction = 1;
     while(true){
         switchBit(PORTB, 0);
         _delay_ms(1000);
+        sleep = sleep + direction;
+        if(direction == 1){
+            if(sleep > 1000){
+                direction = -1;
+            }
+        }else{
+            if(sleep <= 1){
+                direction = 1;
+            }
+        }
     }
 }
 
