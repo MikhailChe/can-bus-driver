dbd889abf469618654d17faaa640fa6223213d75
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2013-10-24 21:11:42 +0600

    PWM tests2

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@ Author: Mikhail Chernoskutov
 */
 #define F_CPU 1000000
 #include "timer.cpp"
+#include "portwork.cpp"
 #include <util/delay.h>
 
 int main(void){
     enable0BPWM();
-    DDRB = 1;
+    DDRB = 0b11111111;
     PORTB = 0b10101010;
     while(1){
         set0Bduty(get0Bduty()-1);
