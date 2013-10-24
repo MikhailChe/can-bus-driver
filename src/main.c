09bdad1b9404be14eb66b1a5cc121b5bee769dd9
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2013-10-24 19:07:59 +0600

    Testing. Nothing works as intended. stupid _delay_ms

--- src/main.c
+++ src/main.c
@@ -7,22 +7,10 @@ Author: Mikhail Chernoskutov
 #include <util/delay.h>
 
 int main(void){
-    setBit(DDRB, 0);
-    double sleep = 0;
-    int direction = 1;
+    setBit(DDRB, 1);
     while(1){
         switchBit(PORTB, 0);
-        _delay_ms(1000);
-        sleep = sleep + direction;
-        if(direction == 1){
-            if(sleep > 1000){
-                direction = -1;
-            }
-        }else{
-            if(sleep <= 1){
-                direction = 1;
-            }
-        }
+        _delay_ms(500);
     }
 }
 
