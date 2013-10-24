122dc459ca74d41e01db5c4fe4128c1ebc5c091f
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2013-10-24 22:13:39 +0600

    Testing math and double

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,23 @@ Author: Mikhail Chernoskutov
 #include "timer.cpp"
 #include "portwork.cpp"
 #include <util/delay.h>
-
+#include <math.h>
 int main(void){
     enable0BPWM();
     DDRB = 0;
     PORTB = 0b11111111;
+    double angle = 0;
     while(1){
-        set0Bduty(get0Bduty()-1);
+        /*set0Bduty(get0Bduty()-1);
         if(get0Bduty()<1){
             set0Bduty(255);
+        }*/
+        set0Bduty((sin(angle)+1)*127);
+        angle += 0.1;
+        if(angle > M_PI*2){
+            angle = 0;
         }
-        _delay_ms(10);
+        _delay_ms(1);
     }
 }
 
