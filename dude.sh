805f3660ad784cfc2a7540f68a37bae4b2d36994
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2013-10-24 18:15:32 +0600

    Added avrdude sh caller

---
+++ dude.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MCU="t2313"
+PROG="linuxspi"
+PORT="/dev/spidev0.0"
+FLELF="./bin/prog.elf"
+BAUD=100000
+sudo avrdude -c $PROG -p $MCU -P $PORT -b $BAUD -U flash:w:$FLELF
+echo "Dude.sh done"
